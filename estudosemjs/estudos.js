1156c4c2cc7cdf9a4da88714270c446a73832227
Marcos da Luz <108414450+NerusNexuss@users.noreply.github.com>
2023-02-12 17:44:49 -0300

    add js exercises

---
+++ estudosemjs/estudos.js
@@ -0,0 +1,9 @@
+var a = 12.0; 
+//a = 'node'  
+//a = true 
+
+a = {num: 12, nome: "zemba",teste: "gato"} 
+var apelido = "ola mundo"
+
+console.log("Nome:" + a.nome) 
+console.log(`nome: ${a.nome} numero: ${a.num}o marcos é: ${a.teste} opa ${apelido}`)
---
+++ estudosemjs/exercicio01.js
@@ -0,0 +1,12 @@
+var b = 0
+
+for (var i = 1; i <= 10; i++) {
+    var o = i
+
+
+    for (var l = 0; l < 10; l++) {
+        var tabuada = o * l
+        console.log(o, "x", l, "=", tabuada) 
+    } 
+    console.log("\n")
+}
