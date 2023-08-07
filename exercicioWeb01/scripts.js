23a903fd6e20ae8fd8c701cda32ff82850628c72
Marcos da Luz <108414450+NerusNexuss@users.noreply.github.com>
2023-08-07 14:12:44 -0300

    Add files via upload

---
+++ exercicioWeb01/scripts.js
@@ -0,0 +1,7 @@
+//Faça uma página HTML com um campo de texto, um botão e um parágrafo. Ao
+//clicar no botão, o texto do campo de texto deve ser apresentado no parágrafo.
+function AddConteudo(){
+    document.getElementById("addtexto").innerText = "oi";
+}
+
+
